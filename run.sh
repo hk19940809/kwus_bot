57dc4d24a31543475d81862329ae871d6a524163
Kotaro Hashimoto <hk19940809@gmail.com>
2021-10-15 05:25:20 +0900

    feat: add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while read line
+do
+  if test "$line" != "" ; then
+    heroku config:set $line
+  fi
+done < ./.env
+heroku config:set GOOGLE_APPLICATION_CREDENTIALS=google-credentials.json
+heroku config:set GOOGLE_CREDENTIALS="$(< ./google-credentials.json)"
+heroku run python3 bot.py
